2ce20d0f92f1cb7d4d79705e0e41707edc54923f
Chris Hanna <chris@beamable.com>
2025-09-11 14:05:37 -0400

    add literate-nav to setup, and allow customization of python location

--- setup.sh
+++ setup.sh
@@ -2,24 +2,27 @@
 
 # Install Python dependencies for mkdocs
 
+PYTHON="python" #note, on mac, it may be "python3"
+PIP="pip"
+
 main() {
     
     # Chek if python is installed
-    if ! command -v python &> /dev/null
+    if ! command -v $PYTHON &> /dev/null
     then
         echo "Python is not installed"
         exit 1
     fi
     
     # Check if pip is installed
-    if ! command -v pip &> /dev/null; then
+    if ! command -v $PIP &> /dev/null; then
         echo "⚠️ pip not found. Attempting to install..."
         curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-        python get-pip.py
+        $PYTHON get-pip.py
         rm get-pip.py
     fi
     
-    pip install mkdocs-material mkdocs-glightbox mkdocs-autorefs mike
+    $PIP install mkdocs-material mkdocs-glightbox mkdocs-autorefs mkdocs-literate-nav mike
   
 }
 
