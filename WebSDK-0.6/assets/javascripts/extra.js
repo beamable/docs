113b673583de2c68164292f20de2df8a2a2e9f78
Vitor Balbio <vitornalbio@gmail.com>
2025-09-17 15:18:39 -0300

    Deployed e9fe6ce to WebSDK-0.6 with MkDocs 1.6.1 and mike 2.1.3

---
+++ WebSDK-0.6/assets/javascripts/extra.js
@@ -0,0 +1,43 @@
+﻿// Runs once the Version element is created by the mike scripts
+function WaitForVersionNav(selector, callback) {
+    const interval = setInterval(() => {
+        const element = document.querySelector(selector);
+        if (element !== null) {
+            clearInterval(interval);
+            callback(element);
+        }
+    }, 500); // Check each 500ms
+}
+WaitForVersionNav('.md-version__list', function(element) {
+    console.log("Elemento encontrado:");
+    console.log(element)
+
+    var unrealIndex = -1;
+    var unityIndex = -1;
+
+    const items = element.children;
+    for (let i = 0; i < items.length; i++) {
+        if(items[i].innerHTML.includes("Unity")){unityIndex = i;break;}
+    }
+
+    for (let j = 0; j < items.length; j++) {
+        if(items[j].innerHTML.includes("Unreal")){unrealIndex = j;break;}
+    }
+
+    // Unity Header
+    if(unityIndex !== -1) {
+        var unityHeader = document.createElement("div");
+        unityHeader.className = "md-version__header";
+        unityHeader.innerText = "Unity SDK";
+        element.insertBefore(unityHeader, items[unityIndex]);
+    }
+
+    // Unreal Header
+    if(unrealIndex !== -1) {
+        var unrealHeader = document.createElement("div");
+        unrealHeader.className = "md-version__header";
+        unrealHeader.innerText = "Unreal SDK";
+        element.insertBefore(unrealHeader, items[unrealIndex]);
+    }
+
+});
