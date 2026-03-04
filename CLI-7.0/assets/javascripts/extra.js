488edc643da156220d82019a01f816a7550e4554
cdhanna <cdhanna@users.noreply.github.com>
2026-03-04 20:06:12 +0000

    Deployed ffffd0e to CLI-7.0 with MkDocs 1.6.1 and mike 2.1.3

--- CLI-7.0/assets/javascripts/extra.js
+++ CLI-7.0/assets/javascripts/extra.js
@@ -9,6 +9,9 @@ if (window.location.pathname.includes('Unreal')) {
     VERSION_NAME = 'WebSDK'
 }
 
+// We can't use the same solution because CLI will be in all the branches. Let's hardcode it for this
+VERSION_NAME = "CLI"
+
 // ---- Helpers ---------------------------------------------------------------
 function WaitForVersionNav(selector, callback) {
     const interval = setInterval(() => {
@@ -79,7 +82,7 @@ WaitForVersionNav('.md-version__list:not(.beam-sdk__list)', function(element) {
 
     // after filtering, strip off the sdk name, because it is extranous
     const subNodes = document.querySelectorAll('.md-header__topic .md-version *');
-    const textReplacements = ['Unity-', 'Unreal-', 'WebSDK-', 'CLI-']
+    const textReplacements = ['Unity-', 'Unreal-', 'WebSDK-', 'CLI-'];
     for (var i = 0 ; i < subNodes.length; i ++){
         const subNode = subNodes[i];
         if (subNode.tagName == 'BUTTON' || subNode.tagName == 'A'){
